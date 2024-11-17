25753ed06d8ea3b30fd6cebdd0f839388e538283
Marlon Ansale <117570623+luffydgreat90@users.noreply.github.com>
2024-11-17 21:41:06 +0000

    start

---
+++ pokedex/pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by marlon von ansale on 17/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
